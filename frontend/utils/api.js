9ab7e163c34b2f8ba1e2e8b40f68df30794a23e1
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-08 22:52:26 +0200

    Update api.js

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -8,7 +8,17 @@ export function getStrapiURL(path) {
 export async function fetchAPI(path) {
   const requestUrl = getStrapiURL(path);
   try {
-    const response = await fetch(requestUrl);
+    const response = await fetch(requestUrl,
+      {
+        method: "GET",
+        headers: {
+          // update with your user-agent
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+          Accept: "application/json; charset=UTF-8",
+        },
+      }
+);
     const data = await response.json();
     return data;    
   } catch (error) {
