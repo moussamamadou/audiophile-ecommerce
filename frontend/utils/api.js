76ccb98c75d85b6196b86001581e76a03883cbaa
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-08 22:56:53 +0200

    update

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,52 +1,49 @@
 export function getStrapiURL(path) {
   return `${
     process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
-  }${path}`;
+  }${path}`
 }
 
 // Helper to make GET requests to Strapi
 export async function fetchAPI(path) {
-  const requestUrl = getStrapiURL(path);
+  const requestUrl = getStrapiURL(path)
   try {
-    const response = await fetch(requestUrl,
-      {
-        method: "GET",
-        headers: {
-          // update with your user-agent
-          "User-Agent":
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
-          Accept: "application/json; charset=UTF-8",
-        },
-      }
-);
-    const data = await response.json();
-    return data;    
+    const response = await fetch(requestUrl, {
+      method: "GET",
+      headers: {
+        // update with your user-agent
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+        Accept: "application/json; charset=UTF-8",
+      },
+    })
+    const data = await response.json()
+    return data
   } catch (error) {
     console.log(error)
   }
 }
 
 export async function getCategories() {
-  const categories = await fetchAPI("/categories");
-  return categories;
+  const categories = await fetchAPI("/categories")
+  return categories
 }
 
 export async function getCategory(slug) {
-  const categories = await fetchAPI(`/categories?slug=${slug}`);
-  return categories?.[0];
+  const categories = await fetchAPI(`/categories?slug=${slug}`)
+  return categories?.[0]
 }
 
 export async function getProducts() {
-  const products = await fetchAPI("/products");
-  return products;
+  const products = await fetchAPI("/products")
+  return products
 }
 
 export async function getProduct(slug) {
-  const products = await fetchAPI(`/products?slug=${slug}`);
-  return products?.[0];
+  const products = await fetchAPI(`/products?slug=${slug}`)
+  return products?.[0]
 }
 
-// Get Home Page 
+// Get Home Page
 export async function getHomePage() {
   const global = await fetchAPI(`/home-page`)
   return global
