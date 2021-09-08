9ffe824c506a5d2f5482e3db9b631ab4105b88a2
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-08 17:02:16 +0200

    Update basic elements components

--- frontend/components/elements/Footer.js
+++ frontend/components/elements/Footer.js
@@ -4,17 +4,16 @@ import Link from "next/link"
 
 const Footer = ({ footer }) => {
   return (
-    <footer>
-      <nav className="border-gray-200 border-b-2 py-6 sm:py-2 bg-dark">
-        <div className="container flex flex-col items-center justify-between">
+    <footer className="mt-10">
+      <nav className="border-gray-200 border-b-2 py-10 bg-dark">
+        <div className="container flex flex-col items-center justify-between gap-5">
           {/* Content aligned to the left */}
-          <div className="flex flex-row items-center w-full justify-between h-20 ">
+          <div className="flex flex-col lg:flex-row  items-center w-full justify-between h-20 ">
             <Link href="/">
               <a className="h-8 w-32 flex items-center justify-center">
                 <CustomImage width="120" height="33" media={footer.logo} />
               </a>
             </Link>
-            {/* List of links on desktop */}
             <ul className="hidden list-none md:flex flex-row gap-4 items-center justify-center">
               {footer.link.map((footerLink) => (
                 <li key={footerLink.id}>
@@ -28,9 +27,9 @@ const Footer = ({ footer }) => {
             </ul>
           </div>
           <div className="flex flex-col gap-5">
-            <div className=" text-white font-extralight grid grid-cols-2">
+            <div className=" text-white font-extralight grid grid-cols-1 lg:grid-cols-2 gap-10">
               <div className=" opacity-50 ">{footer.bigText}</div>
-              <div className="flex gap-5 justify-end items-center">
+              <div className="flex gap-5 justify-center lg:justify-end items-center">
                 <Link href="/">
                   <a>
                     <CustomImage src='/github.svg'  height={30} width={30}  />
@@ -48,7 +47,7 @@ const Footer = ({ footer }) => {
                 </Link>
               </div>
             </div>
-            <div className="text-white font-bold opacity-50">
+            <div className="text-white font-bold text-center lg:text-left opacity-50">
               <div className="">{footer.smallText}</div>
             </div>
           </div>
--- frontend/components/elements/Navbar.js
+++ frontend/components/elements/Navbar.js
@@ -4,11 +4,10 @@ import { useRouter } from "next/router"
 import CustomImage from "./CustomImage"
 import CustomLink from "./CustomLink"
 import CategoryLink from "../section/CategoryLink"
+import { Menu, Transition } from "@headlessui/react"
 
 const Navbar = ({ navbar, categoryLink }) => {
   const router = useRouter()
-  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false)
-
   return (
     <>
       {/* The actual navbar */}
@@ -16,13 +15,66 @@ const Navbar = ({ navbar, categoryLink }) => {
         <div className="container flex flex-row items-center justify-between">
           {/* Content aligned to the left */}
           <div className="flex flex-row items-center w-full justify-between   h-20">
+            <div className="lg:hidden">
+              <Menu>
+                {({ open }) => (
+                  <>
+                    <Menu.Button>
+                      <CustomImage src="/menu.svg" height={30} width={30} />
+                    </Menu.Button>
+                    <Transition
+                      show={open}
+                      enter="transition duration-100 ease-out"
+                      enterFrom="opacity-0"
+                      enterTo=" opacity-100"
+                      leave="transition duration-75 ease-out"
+                      leaveFrom=" opacity-100"
+                      leaveTo=" opacity-0"
+                    >
+                      <Menu.Items>
+                        <div className=" w-full bg-white absolute top-[95px] left-0 pb-10">
+                          <div className="container grid grid-cols-1 md:grid-cols-3 gap-20 md:gap-5 py-16 relative">
+                            {categoryLink.linkImage.map((mylink) => (
+                              <Menu.Item>
+                                <div
+                                  className="relative flex flex-col justify-between items-center h-full z-10"
+                                  key={mylink.id}
+                                >
+                                  <div className="absolute h-2/3 w-full bottom-[-35px] left-0 bg-neutral rounded-lg z-0"></div>
+                                  <div className="w-1/2">
+                                    <CustomImage media={mylink.image} />
+                                  </div>
+                                  <div className=" relative">
+                                    <h6>{mylink.text}</h6>
+                                    <Link href={mylink.url}>
+                                      <a className=" flex justify-center items-center mt-2 z-10">
+                                        <span>Shop </span>
+                                        <CustomImage
+                                          src="/arrow-right.svg"
+                                          height={20}
+                                          width={20}
+                                        />
+                                      </a>
+                                    </Link>
+                                  </div>
+                                </div>
+                              </Menu.Item>
+                            ))}
+                          </div>
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </>
+                )}
+              </Menu>
+            </div>
             <Link href="/">
               <a className="h-8 w-32 flex items-center justify-center">
                 <CustomImage width="120" height="33" media={navbar.logo} />
               </a>
             </Link>
             {/* List of links on desktop */}
-            <ul className="-ml-16 hidden list-none md:flex flex-row gap-4 items-center justify-center">
+            <ul className="-ml-16 hidden list-none lg:flex flex-row gap-4 items-center justify-center">
               {navbar.link.map((navLink) => (
                 <li key={navLink.id}>
                   <CustomLink link={navLink}>
@@ -33,29 +85,15 @@ const Navbar = ({ navbar, categoryLink }) => {
                 </li>
               ))}
             </ul>
-            <a className="text-white" href="">
-              <CustomImage src='/cart.svg' height={25} width={25} />
-            </a>
-          </div>
-          <div className="flex">
-            <div className="md:hidden">
-              Menu
-            </div>
+            <button class="snipcart-checkout">
+              <CustomImage src="/cart.svg" height={25} width={25} />
+              <span class="snipcart-items-count bg-primary text-sm font-bold rounded-full text-white px-2 py-1 absolute top-[15px] -ml-3"></span>
+            </button>
           </div>
         </div>
       </nav>
-
-      {/* Mobile navigation menu panel */}
-      {/* {mobileMenuIsShown && (
-        <MobileNavMenu
-          navbar={navbar}
-          closeSelf={() => setMobileMenuIsShown(false)}
-      <CategoryLink categoryLink={ categoryLink }/>
-        />
-      )} */}
     </>
   )
 }
-
 
 export default Navbar
