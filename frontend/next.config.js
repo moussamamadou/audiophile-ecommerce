04c0c1becc037f3ae709be068c3d48e370edc824
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-08 17:00:15 +0200

    Update Config

---
+++ frontend/next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+    images: {
+        domains: ["res.cloudinary.com"],
+    },
+  }
+  
+  module.exports = nextConfig
