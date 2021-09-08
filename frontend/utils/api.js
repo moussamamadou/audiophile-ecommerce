857f06a41382696accc1acbee011cfcbaf331e5b
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-08 23:10:55 +0200

    Deploy

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -11,9 +11,10 @@ export async function fetchAPI(path) {
     const response = await fetch(requestUrl, {
       method: "GET",
       headers: {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
-        "Accept": "application/json",
-        "Content-Type" : "application/json",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
     })
     const data = await response.json()
